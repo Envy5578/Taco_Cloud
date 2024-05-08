ed2d8beef7491d02391673516d46d18545a36936
Andrey Zozuk <andreyzozuk@gmail.com>
2024-05-08 19:47:37 +0400

    feat: create fields class

--- src/main/java/com/example/taco_cloud/Model/TacoOrder.java
+++ src/main/java/com/example/taco_cloud/Model/TacoOrder.java
@@ -1,5 +1,23 @@
 package com.example.taco_cloud.Model;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
 
+@Data
 public class TacoOrder {
-    
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private final List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        this.tacos.add(taco);
+    }
+
 }
