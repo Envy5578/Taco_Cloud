1a6e9013a7888782fc133c8d458a1890b3d69c2c
Andrey Zozuk <andreyzozuk@gmail.com>
2024-05-08 19:02:34 +0400

    feat: add field class Ingredient

--- src/main/java/com/example/taco_cloud/Model/Inredient.java
+++ src/main/java/com/example/taco_cloud/Model/Inredient.java
@@ -1,6 +1,11 @@
 package com.example.taco_cloud.Model;
 
+import lombok.Data;
 
+@Data
 public class Inredient {
-    
+    private final String id;
+    private final String name;
+    private final Type type;
+    public enum Type {WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE}
 }
