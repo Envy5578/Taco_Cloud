817f1f28d6d972586edee7916afdf7e53140655f
Andrey Zozuk <andreyzozuk@gmail.com>
2024-05-08 19:30:57 +0400

    feat: create fields

--- src/main/java/com/example/taco_cloud/Model/Taco.java
+++ src/main/java/com/example/taco_cloud/Model/Taco.java
@@ -1,5 +1,12 @@
 package com.example.taco_cloud.Model;
 
+import java.util.List;
+
+
+import lombok.Data;
+
+@Data
 public class Taco {
-    
+    private String name;
+    private List<Inrgedient> ingredients;
 }
